3dda8914e91a37df4cb51e003afa894640bf4ed2
chrisesquerra <esquerra>
2018-11-12 13:55:10 -0600

    Updates to outfit functions

--- src/main/java/com/codeup/closetrycorner/controllers/OutfitsController.java
+++ src/main/java/com/codeup/closetrycorner/controllers/OutfitsController.java
@@ -1,23 +1,37 @@
 package com.codeup.closetrycorner.controllers;
 
+import com.codeup.closetrycorner.models.Outfit;
+import com.codeup.closetrycorner.models.User;
 import com.codeup.closetrycorner.services.CatSvc;
 import com.codeup.closetrycorner.services.GarmentSvc;
+import com.codeup.closetrycorner.services.OutfitsSvc;
 import com.codeup.closetrycorner.services.UserSvc;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OutfitsController {
     private GarmentSvc garmentSvc;
     private UserSvc userSvc;
     private CatSvc catSvc;
-//    private OutfitSvc outfitSvc;
+    private OutfitsSvc outfitsSvc;
 
 
 
 
-//    @GetMapping("/outfits")
-//    public String showAllOutfits(Model vModel){
-//        vModel.addAttribute("outfits", outfitSvc.findAll());
-//        return "/closet/outfits";
-//    }
+    @GetMapping("/outfits")
+    public String showAllOutfits(Model vModel){
+        vModel.addAttribute("outfits", outfitsSvc.findAll());
+        return "/closet/outfits";
+    }
+
+    @GetMapping("/outfits/create")
+    public String createOutfitForm(Model vModel){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        vModel.addAttribute("outfit", new Outfit());
+        vModel.addAttribute("garments", garmentSvc.findAllForUser(user));
+                return "redirect:/outfits/create";
+    }
 }
