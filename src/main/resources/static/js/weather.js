b0560903e8265a2caa1421817520adce73433f4e
Stephanie Velazquez <velazquez.stephanie02@gmail.com>
2018-11-16 13:35:41 -0600

    feat: weather function

---
+++ src/main/resources/static/js/weather.js
@@ -0,0 +1,23 @@
+(function () {
+    $.get("http://api.openweathermap.org/data/2.5/weather", {
+        APPID: "09be3d87503af2062f84723988428f58",
+        q:     "San Antonio, US",
+        units: "imperial"
+    })
+        .done(function (data) {
+            console.log(data);
+            let currentTemp = data.main.temp;
+            let generalWeather = data.weather[0].main;
+            let icon = data.weather[0].icon;
+            $('#weather-box').append("<h5><img src='http://openweathermap.org/img/w/" + icon + ".png'/>" + " It is " + currentTemp + " degrees and " + generalWeather + " in " + data.name + "</h5>");
+            if(currentTemp < 60){
+                $('#weather-box').append("<p>You may need a jacket. Try searching for cold weather</p>");
+            } else if(currentTemp > 80){
+                $('#weather-box').append("<p>You may need a hat. Try searching for hot weather</p>");
+            }
+            if(generalWeather == "Rain"){
+                $('#weather-box').append("<p>You may need an umbrella</p>");
+            }
+
+        });
+})();
