aa9ed2a6a8bd5fb63a25ff3b505b5f982b364f78
chrisesquerra <esquerrac@gmail.com>
2018-11-18 16:30:20 -0600

    added 35 new garments to seeder database with categories.  2 new outfits created using new garments.  Multiple outfits now tile like garments do, but content overflowing.  Will continue to work on it.

--- seeder.sql
+++ seeder.sql
@@ -1,9 +1,9 @@
 use closet_db;
 insert into users (username, email, password)
 values(
-          'BigParrot',
+          'Sally',
           'BirdGuy@email.com',
-          'password'
+          '$2a$10$Eg/5tllHhpbqZs6A3PhUpuN57jd7gseai7VBA95BDt6bnTtitS/WC'
           ),(
           'RainbowUnicorn',
           'colors@email.com',
@@ -42,7 +42,7 @@ values(
 ),(
 'Pants'
 ),(
-'Shorts'
+'Shorts/Skirt'
 ),(
 'Leggings'
 ),(
@@ -80,28 +80,127 @@ values(
 
 
 
-insert into garments (date, image, user_id)
+insert into garments (image, user_id)
 values (
-'22 October 2018',
 'https://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw0b78138c/images/hi-res/27805_COI.jpg?sw=750&sh=750&sm=fit&sfrm=png',
 1
 ),(
-'24 October 2018',
 'https://www.substation-safety.com/wp-content/uploads/2018/05/tranemo-5940-92-FR-T-shirt-Long-sleeves-750x750.jpg',
 1
 ),(
-'22 October 2018',
 'https://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwe7863b74/images/hi-res/55910_MJVK.jpg?sw=750&sh=750&sm=fit&sfrm=png',
 1
 ),(
-'22 October 2018',
 'https://www.larsmode.com/image/cache/catalog/LM4_6861-750x750.jpg',
 1
 ),(
-'22 October 2018',
 'https://www.jubidubi.com/image/cache/wkseller/BEING%20353/style87/87-PRINTED-WHITE-750x750.jpg',
 1
-);
+),(
+'https://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwc19a496d/images/hi-res/82156_FGE.jpg?sw=750&sh=750&sm=fit&sfrm=png',
+1
+),(
+'https://images.thenorthface.com/is/image/TheNorthFace/NF0A2TCT_254_hero?$638x745$',
+1
+),(
+'https://www.sevsafe.co.uk/image/cache/catalog/Products/Tranemo/1129%20Comfort%20Light%20Ladies%20Trousers/1129%20Comfort%20Light%20Ladies%20Trousers%20(Black)-750x750.jpg',
+1
+),(
+'https://digital-world-online.com/image/cache/catalog/Softline/CL170807W01/CL170807W01-750x750.jpg',
+1
+),(
+'https://www.netflea.com/media/catalog/product/cache/2/image/750x750/9df78eab33525d08d6e5fb8d27136e95/D/S/DSC_04221521389156.JPG',
+1
+),(
+'https://www.dhresource.com/0x0s/f2-albu-g5-M00-B8-75-rBVaJFisg5mAMio3AALEvnIjw5E197.jpg/wholesale-red-and-black-plaid-shirt-men-shirts.jpg',
+1
+),(
+'https://dsw.scene7.com/is/image/DSWShoes/429390_201_ss_01?$colpg$',
+1
+),(
+'https://lp.stories.com/app005prod?set=source[02_0230430_005_001],type[PRODUCT],device[hdpi],quality[80],ImageVersion[2018082]&call=url[file:/product/style]',
+1
+),(
+'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/1/_103461881.jpg?h=365&w=240&dpr=2&quality=45&fit=fill&fm=jpg',
+1
+),(
+'https://c.static-nike.com/a/images/t_PDP_1280_v1/f_auto/j9cqrpaufkz5adxnspmj/tanjun-womens-shoe-ZwTJEKnB.jpg',
+1
+),(
+'http://www.kaiad.org/image/cache/data/category_47/men_shirts_mens_oxford_dress_shirts_casual_slim_fit_button_down_long_sleeve_shirts_white_mens_clothing_o7woprydd-600x521.jpg',
+1
+),(
+'https://dsw.scene7.com/is/image/DSWShoes/429109_001_ss_01?$colpg$',
+1
+),(
+'https://images-na.ssl-images-amazon.com/images/I/61cKCESBAwL._UX466_.jpg',
+1
+),(
+'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/16/_103759216.jpg?h=365&w=240&dpr=2&quality=45&fit=fill&fm=jpg',
+1
+),(
+'https://www.pacsun.com/dw/image/v2/AAJE_PRD/on/demandware.static/-/Sites-pacsun_storefront_catalog/default/dweaa1516a/product_images/0170436750012NEW_00_065.jpg?sw=458&sh=710&sm=fit',
+1
+),(
+'https://ec7a5588ae93a1bb130d-0e290dfc07d515b4e8d8a66a520e8e10.ssl.cf2.rackcdn.com/product-hugerect-505138-190870-1431274722-f8534e7bd6d1834cdb721a92e0dd812c.jpg',
+1
+),(
+'https://www.oldbullshorts.com/pub/media/catalog/product/cache/7643e01b9d2c817c5245c606d57afe88/2/0/209laydown.jpg',
+1
+),(
+'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/10/_13418190.jpg?h=365&w=240&dpr=2&quality=45&fit=fill&fm=jpg',
+1
+),(
+'http://www.mtlindesayview.com/images/882/1998551928985551.jpg',
+1
+),(
+'http://cdn.shopify.com/s/files/1/0684/2619/products/skirts-safia-stretch-african-print-pencil-midi-skirt-with-peplum-hem-purple-ovals-1_grande.jpg?v=1519073430',
+1
+),(
+'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/8/_103461808.jpg?h=365&w=240&dpr=2&quality=45&fit=fill&fm=jpg',
+1
+),(
+'https://lp2.hm.com/hmgoepprod?set=source[/2d/55/2d5569e9b3765cf91d3ee4358c9bdccd41eda2e3.jpg],origin[dam],category[ladies_shorts_jeans],type[DESCRIPTIVESTILLLIFE],res[s],hmver[1]&call=url[file:/product/main]',
+1
+),(
+'http://cdn.shopify.com/s/files/1/1312/5379/products/northernlightsscarf_grande.jpg?v=1471380247',
+1
+),(
+'http://www.arriendosvillarrica.com/image/cache/data/category_20/oas-no-bad-vibes-swimming-shorts-men-s-swimwear-jocvgr8b-2418-600x600_0.jpg',
+1
+),(
+'https://i.pinimg.com/736x/c2/e5/b9/c2e5b90d82d1d5b204fbd09f90a130f9--vintage-rehearsal-dinners-rehearsal-dinner-dresses.jpg',
+1
+),(
+'https://evontte.com/wp-content/uploads/2016/08/Coco-Tan-BTT22618-Front-Evontte-Handbags-copy.jpg',
+1
+),(
+'https://i.pinimg.com/originals/80/d8/af/80d8af622ddeeac033995f1468484b99.jpg',
+1
+),(
+'https://www.costco.co.uk/medias/sys_master/products/hc4/hb5/10054735233054.jpg',
+1
+),(
+'https://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/images/2017/12/_101402240.jpg?itok=uiHZyShl',
+1
+),(
+'https://i.pinimg.com/originals/32/1f/3a/321f3a49e8fd2dc3963d7bd099636ce4.jpg',
+1
+),(
+'https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2017/09/12/goods-first-img/1512957990612978009.jpg',
+1
+),(
+'https://cdn.lookastic.com/blue-cotton-blazer/blue-slim-fit-cotton-twill-suit-jacket-original-162382.jpg',
+1
+),(
+'https://riverisland.scene7.com/is/image/RiverIsland/299302_main?wid=1200',
+1
+),(
+'https://ae01.alicdn.com/kf/HTB1MqCrNpXXXXXDXXXXq6xXFXXXp/sweaters-women-harajuku-style-2018-korean-kawaii-winter-cartoon-pullover-stripes-cute-embroidery-racket-oversized-sweater.jpg',
+1
+),(
+'http://cdn.shopify.com/s/files/1/2343/4325/products/product-image-402753337_grande.jpg?v=1506697688',
+1);
 
 
 insert into garments_categories (garment_id, category_id)
@@ -124,7 +223,132 @@ values
 (5,1),
 (5,17),
 (5,19),
-(5,21);
+(5,21),
+(6,5),
+(6,15),
+(6,17),
+(6,20),
+(6,22),
+(7,5),
+(7,17),
+(7,19),
+(8,5),
+(8,15),
+(8,17),
+(9,5),
+(9,17),
+(10,5),
+(10,17),
+(10,19),
+(10,21),
+(11,1),
+(11,13),
+(11,17),
+(12,16),
+(12,17),
+(12,19),
+(13,10),
+(13,15),
+(13,14),
+(13,19),
+(14,4),
+(14,15),
+(14,17),
+(14,21),
+(15,16),
+(15,17),
+(15,20),
+(16,1),
+(16,17),
+(16,19),
+(16,21),
+(17,16),
+(17,17),
+(17,19),
+(18,6),
+(18,17),
+(19,4),
+(19,14),
+(19,17),
+(20,6),
+(20,12),
+(20,17),
+(20,13),
+(21,12),
+(21,17),
+(21,13),
+(22,6),
+(22,13),
+(22,17),
+(23,10),
+(23,13),
+(24,12),
+(24,17),
+(24,13),
+(25,6),
+(25,13),
+(25,15),
+(25,17),
+(25,19),
+(26,4),
+(26,15),
+(26,17),
+(27,6),
+(27,13),
+(27,17),
+(28,10),
+(28,13),
+(28,15),
+(28,17),
+(29,6),
+(29,13),
+(29,12),
+(29,17),
+(30,8),
+(30,13),
+(30,18),
+(30,21),
+(31,11),
+(31,17),
+(32,8),
+(32,13),
+(32,17),
+(32,19),
+(32,21),
+(33,11),
+(33,18),
+(33,19),
+(33,21),
+(34,11),
+(34,18),
+(34,19),
+(34,21),
+(35,8),
+(35,13),
+(35,18),
+(35,21),
+(36,2),
+(36,14),
+(36,15),
+(36,17),
+(37,2),
+(37,15),
+(37,17),
+(37,19),
+(37,18),
+(38,2),
+(38,15),
+(38,17),
+(38,19),
+(38,18),
+(39,2),
+(39,14),
+(39,15),
+(39,17),
+(40,16),
+(40,17),
+(40,15);
+
 
 
 insert into outfit (date, description, user_id)
@@ -132,7 +356,14 @@ values(
 '22 October 2018',
 'Sweet Threads',
 1
-);
+),(
+'18 October 2018',
+'Laundry Day',
+1
+),(
+'2 November 2018',
+'Shopping',
+1);
 
 
 
@@ -141,27 +372,11 @@ values
 (1,1),
 (1,2),
 (1,3),
-(1,4);
-
-insert into garments (date, image, user_id)
-values (
-           '22 October 2018',
-           'https://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw0b78138c/images/hi-res/27805_COI.jpg?sw=750&sh=750&sm=fit&sfrm=png',
-           1
-           ),(
-           '24 October 2018',
-           'https://www.substation-safety.com/wp-content/uploads/2018/05/tranemo-5940-92-FR-T-shirt-Long-sleeves-750x750.jpg',
-           1
-           ),(
-           '22 October 2018',
-           'https://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwe7863b74/images/hi-res/55910_MJVK.jpg?sw=750&sh=750&sm=fit&sfrm=png',
-           1
-           ),(
-           '22 October 2018',
-           'https://www.larsmode.com/image/cache/catalog/LM4_6861-750x750.jpg',
-           1
-           ),(
-           '22 October 2018',
-           'https://www.jubidubi.com/image/cache/wkseller/BEING%20353/style87/87-PRINTED-WHITE-750x750.jpg',
-           1
-           );
+(1,4),
+(2,7),
+(2,11),
+(2,17),
+(3,36),
+(3,31),
+(3,12),
+(3,9);
